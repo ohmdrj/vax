54e6000db7f349918e090c5637929109f6837e36
pikl <jan.pikl@marbes.cz>
2016-01-21 15:24:56 +0100

    AxBinder constructor

--- src/main/java/cz/req/ax/AxBinder.java
+++ src/main/java/cz/req/ax/AxBinder.java
@@ -2,7 +2,9 @@ package cz.req.ax;
 
 import com.google.common.base.Strings;
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.util.BeanItem;
 import com.vaadin.ui.Field;
+import org.springframework.util.Assert;
 
 /**
  * @author <a href="mailto:jan.pikl@marbes.cz">Jan Pikl</a>
@@ -11,11 +13,27 @@ import com.vaadin.ui.Field;
 public class AxBinder<T> extends BeanFieldGroup<T> {
 
     public static <T> AxBinder<T> init(Class<T> beanType) {
-        return new AxBinder<>(beanType);
+        Assert.notNull(beanType);
+        try {
+            return init(beanType.newInstance());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private AxBinder(Class<T> beanType) {
-        super(beanType);
+    public static <T> AxBinder<T> init(T beanValue) {
+        Assert.notNull(beanValue);
+        return new AxBinder<>(new BeanItem<>(beanValue));
+    }
+
+    public static <T> AxBinder<T> init(BeanItem<T> beanItem) {
+        Assert.notNull(beanItem);
+        return new AxBinder<>(beanItem);
+    }
+
+    private AxBinder(BeanItem<T> beanItem) {
+        super((Class<T>) beanItem.getBean().getClass());
+        setItemDataSource(beanItem);
         setFieldFactory(new AxFieldFactory());
     }
 
