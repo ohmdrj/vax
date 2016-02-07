f4b47d6a57dd2462c783305794f341b51c9bfc18
pikl <jan.pikl@marbes.cz>
2016-02-07 21:00:13 +0100

    Example

--- src/test/java/cz/req/ax/FieldBuildersTest.java
+++ src/test/java/cz/req/ax/FieldBuildersTest.java
@@ -1,5 +1,10 @@
 package cz.req.ax;
 
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Field;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
+
 /**
  * @author <a href="mailto:jan.pikl@marbes.cz">Jan Pikl</a>
  *         Date: 7.2.2016
@@ -16,10 +21,24 @@ public class FieldBuildersTest {
 
     public static void main(String[] args) {
         AxBinder binder = AxBinder.init(TestObj.class);
+
         binder.addText("name").required().maxLength(23).value("Test");
-        binder.addTextArea("description").rows(5);
         binder.addField("check");
+        binder.addTextArea("description").rows(5);
+
         AxFormLayout layout = binder.createForm();
+    }
+
+    public static void main2(String[] args) {
+        AxBinder binder = AxBinder.init(TestObj.class);
+
+        TextField nameField = binder.addText("name").required().maxLength(23).value("Test").field();
+        Field checkField = binder.addField("check").field();
+        TextArea descField = binder.addTextArea("description").rows(5).field();
+
+        AxFormLayout layout = new AxFormLayout();
+        layout.addRow().caption("Name / check").component(new CssLayout(nameField, checkField));
+        layout.addRow(descField);
     }
 
 }
